4bd6310f6c4cf9ab8a92e9011a3fb4cc31aae16d
Rob Gordon <flashg1775@gmail.com>
2016-08-12 17:29:22 +0000

    JS basic part 2

---
+++ week1/browser-js-basics/JS-practice-2.js
@@ -0,0 +1,15 @@
+/*var name = prompt("What's your name?");
+
+var nameAnswer = prompt(name);
+
+function getName() {
+    return nameAnswer;
+}*/
+
+var name = prompt ("What is your name?"); 
+
+function salutation() {
+    alert ("Hello, "+name+"!");
+}
+
+salutation();
