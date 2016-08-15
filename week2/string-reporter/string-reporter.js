251380f56c675fcd1b1319132805879c10576c5d
Rob Gordon <flashg1775@gmail.com>
2016-08-15 21:58:13 +0000

    string-reporter complete

--- week2/string-reporter/string-reporter.js
+++ week2/string-reporter/string-reporter.js
@@ -1,7 +1,9 @@
 var name = prompt("What's your name?");
 
 function hello(){
-    alert ("Hey, " + name + "!" + "\n" + "You're name is " + name.length+ " letters long.");
+    alert ("Hey, " + name + "!" + "\n" + "Your name is " + name.length+ " letters long." +"\n"+ "The third letter in your name is " +name.charAt(2) 
+    +"\n"+ "This is what " + name + " looks like in lowercase: "  + name.toLowerCase() +"\n"+ "And in uppercase: " + name.toUpperCase() +"\n"+ "Is " + name + " a common name in your family?"
+    +"\n"+ "subword: " +name.substring(0,3));
 }
 
 hello();
